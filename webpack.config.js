cc818fc1cb7868e915920f2269b87a681179f629
Masaya Kawai <mas@42zz.net>
2020-08-26 14:33:43 +0900

    ie11 polyfill 導入

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+
+module.exports = {
+    // 'development' | 'production'
+    mode: process.env.NODE_ENV,
+    // entry points -> ts ファイルを指定（複数可）
+    // https://webpack.js.org/concepts/entry-points/#multi-page-application
+    entry: {
+        "main": path.resolve(__dirname, "main.ts")
+    },
+    output: {
+        path: path.resolve(__dirname),
+        filename: "[name].bundle.js"
+    },
+    resolve: {
+        extensions: [".ts", ".js"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                // ローダーをチェーン化する場合は 右 -> 左 で処理
+                // https://webpack.js.org/configuration/module/#ruleuse
+                loaders: ["babel-loader", "ts-loader"]
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(["ts/*"])
+    ]
+};
